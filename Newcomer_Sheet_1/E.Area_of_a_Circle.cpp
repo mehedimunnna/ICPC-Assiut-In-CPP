d3df52c3c5468a252050ef98a1fdb15503577a2a
Mehedi Hasan Munna <132080227+mehedimunnna@users.noreply.github.com>
2024-04-19 12:40:58 +0600

    Sheet #1 (Data type - Conditions)

---
+++ Newcomer_Sheet_1/E.Area_of_a_Circle.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+int main()
+{
+    double r, area;
+    cin >> r;
+
+    area = 3.141592653 * r * r;
+    cout << fixed << setprecision(9)<<area<<endl;
+    return 0;
+}
