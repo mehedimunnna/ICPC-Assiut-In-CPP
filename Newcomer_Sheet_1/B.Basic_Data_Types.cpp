9b3a429ae1f66b957af64279a8229dbb6e6c73c6
Mehedi Hasan Munna <132080227+mehedimunnna@users.noreply.github.com>
2024-04-19 11:26:05 +0600

    Sheet #1 (Data type - Conditions)

---
+++ Newcomer_Sheet_1/B.Basic_Data_Types.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a;
+    long long b;
+    char c;
+    float f;
+    double d;
+
+    cin >> a >> b >> c >> f >> d;
+    cout << a << endl;
+    cout << b << endl;
+    cout << c << endl;
+    cout << f << endl;
+    cout << d << endl;
+
+    return 0;
+}
