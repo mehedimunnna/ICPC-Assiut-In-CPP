b69c096f387af7cd51fbdb38b9872f8521c1fe66
Mehedi Hasan Munna <132080227+mehedimunnna@users.noreply.github.com>
2024-04-19 16:37:06 +0600

    Sheet #1 (Data type - Conditions)

---
+++ Newcomer_Sheet_1/K.Max_and_Min.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a, b, c;
+    cin >> a >> b >>c;
+
+    if(a<=b && a<=c)
+        cout << a;  
+    else if(b<=a && b<=c)
+        cout << b;
+    else if(c<=a && c<=b)
+        cout<< c;
+
+    cout << " ";
+
+    if(a>=b && a>=c)
+        cout << a<<endl;  
+    else if(b>=a && b>=c)
+        cout << b<<endl;
+    else if(c>=a && c>=b)
+        cout<< c<<endl;
+
+    return 0;
+}
