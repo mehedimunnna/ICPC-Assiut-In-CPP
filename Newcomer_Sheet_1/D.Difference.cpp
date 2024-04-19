0d2f44badd013b315eb81c9ea7c32b816116ec11
Mehedi Hasan Munna <132080227+mehedimunnna@users.noreply.github.com>
2024-04-19 12:30:47 +0600

    Sheet #1 (Data type - Conditions)

---
+++ Newcomer_Sheet_1/D.Difference.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    long long a, b, c, d, x;
+    cin >> a >> b >> c >> d;
+
+    x = (a * b) - (c * d);
+
+    cout<< "Difference = " << x << endl;
+
+    return 0;
+}
